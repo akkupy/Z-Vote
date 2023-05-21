45da79d2addb628c574569c57172d6c323ea8764
akkupy <akkupy@proton.me>
2023-05-21 12:10:07 +0530

    Added mysql db

--- Z-Vote/blockchain_voting/settings.py
+++ Z-Vote/blockchain_voting/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blockchain_voting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'zvotedb',
+        'PORT': 3306,
     }
 }
 
